5ce499ab8604fbe46aba3636c0c4577a78dfb235
Matt Struble <4325029+mattstruble@users.noreply.github.com>
2025-09-25 06:49:01 -0400

    wip: spacy models

--- despii/detectors/spacy.py
+++ despii/detectors/spacy.py
@@ -1,8 +1,10 @@
-import subprocess
+import subprocess  # nosec B404
 import sys
 from functools import lru_cache
 
 import spacy
+
+from despii.util import detect_system_lang
 
 
 @lru_cache(maxsize=1)
@@ -11,7 +13,7 @@ def _load_spacy_model(model_name: str) -> spacy.language.Language:
         return spacy.load(model_name)
     except Exception:
         _ = subprocess.check_call(
-            [sys.executable, "-m", "spacy", "download", model_name]
+            [sys.executable, "-m", "spacy", "download", model_name]  # nosec B603
         )
         return spacy.load(model_name)
 
@@ -19,25 +21,43 @@ def _load_spacy_model(model_name: str) -> spacy.language.Language:
 def _spacy_model_for_lang(lang_code: str | None) -> str:
     """Map a two-letter lang code to a spaCy core_web_sm model package name.
 
-    Defaults to 'en_core_web_sm' for unknown or unsupported languages.
+    Defaults to 'xx_ent_wiki_sm' for unknown or unsupported languages.
 
     :param lang_code: two-letter language code
     :return: supported spaCy model
     """
-    lang_code = lang_code if lang_code else "en"
-
     mapping = {
-        "en": "en_core_web_sm",
-        "fr": "fr_core_news_sm",
+        "ca": "ca_core_news_sm",
+        "da": "da_core_news_sm",
         "de": "de_core_news_sm",
+        "el": "el_core_news_sm",
+        "en": "en_core_web_sm",
         "es": "es_core_news_sm",
-        "pt": "pt_core_news_sm",
+        "fi": "fi_core_news_sm",
+        "fr": "fr_core_news_sm",
+        "hr": "hr_core_news_sm",
         "it": "it_core_news_sm",
-        "nl": "nl_core_news_sm",
-        "xx": "xx_ent_wiki_sm",
-        "zh": "zh_core_web_sm",
         "ja": "ja_core_news_sm",
+        "ko": "ko_core_news_sm",
+        "lt": "lt_core_news_sm",
+        "mk": "mk_core_news_sm",
+        "nb": "nb_core_news_sm",
+        "nl": "nl_core_news_sm",
+        "pt": "pt_core_news_sm",
+        "ro": "ro_core_news_sm",
         "ru": "ru_core_news_sm",
+        "sl": "sl_core_news_sm",
+        "sv": "sv_core_news_sm",
+        "uk": "uk_core_news_sm",
+        "xx": "xx_ent_wiki_sm",  # multi language
+        "zh": "zh_core_web_sm",
     }
 
-    return mapping.get(lang_code, mapping["en"])
+    return mapping.get(lang_code, mapping["xx"])
+
+
+@lru_cache(maxsize=1)
+def spacy_model() -> spacy.language.Language:
+    lang = detect_system_lang()
+    model_name = _spacy_model_for_lang(lang)
+    return _load_spacy_model(model_name)
